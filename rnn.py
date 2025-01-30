733ad2529468f2797fa9da18c390bfd12fc75806
konradszewczyk <konszewczyk@outlook.com>
2025-01-30 21:15:09 +0100

    Early version of simplified real-valued RGLRU

--- rnn.py
+++ rnn.py
@@ -40,17 +40,60 @@ class RNN(nn.Module):
         return nn.Dense(self.d_out)(h)
 
 
+class RGLRU(nn.Module):
+    H: Hyperparams
+    d_hidden: int
+    d_out: int
+    reverse: bool = False
+
+    @nn.compact
+    def __call__(self, x):
+        # TODO: implement BlockDiagonalLinear from RecurrentGemma
+        batch_size, _, _ = x.shape
+
+        def recurrence_init(rng, shape):
+            min_rad = self.H.rnn_init_minval
+            max_rad = self.H.rnn_init_maxval
+
+            unif = random.uniform(rng, shape=shape)
+            a = 0.5 * jnp.log(unif * (max_rad**2 - min_rad**2) + min_rad**2)
+            return jnp.log(jnp.exp(-a) - 1.0)
+
+        x = nn.Dense(self.d_hidden)(x)
+
+        a_param = self.param("a", recurrence_init, (self.d_hidden,))
+        gate_x = nn.sigmoid(nn.Dense(self.d_hidden)(x))
+        gate_a = nn.sigmoid(nn.Dense(self.d_hidden)(x))
+
+        log_a = -8.0 * gate_a * nn.softplus(a_param)
+        a = jnp.exp(log_a)
+        a_squared = jnp.exp(2 * log_a)
+
+        x = gate_x * x
+
+        def f(h, x):
+            h = a * h + (1 - a_squared) ** 0.5 * (x)
+            return h, h
+
+        init = jnp.zeros((batch_size, self.d_hidden))
+        x = rearrange(x, "batch seq chan -> seq batch chan")
+        _, h = jax.lax.scan(f, init, x, reverse=self.reverse)
+        h = rearrange(h, "seq batch chan -> batch seq chan")
+        return nn.Dense(self.d_hidden)(h)
+
+
 class RNNBlock(nn.Module):
     H: Hyperparams
     d_hidden: int
     d_out: int
     bidirectional: bool = False
     use_residual: bool = False
+    recurrent_block = RGLRU
 
     def setup(self):
         self.forward = RNN(self.H, d_hidden=self.d_hidden, d_out=self.d_out)
         if self.bidirectional:
-            self.backward = RNN(
+            self.backward = self.recurrent_block(
                 self.H, d_hidden=self.d_hidden, d_out=self.d_out, reverse=True
             )
 
