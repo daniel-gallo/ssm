Fix gradient NaNs in complex valued methods + changes to AR

models/autoregressive.py
@@ -1,4 +1,5 @@
 import dataclasses
+from re import X
 
 import flax.linen as nn
 import jax
@@ -40,23 +41,25 @@ class ARHyperparams(Hyperparams):
     pool_temporal: tuple[int, ...] = (4, 4)
     pool_features: tuple[int, ...] = (2, 2)
 
-    rnn_init_minval: float = 0.4
+    rnn_init_minval: float = 0.9
     rnn_init_maxval: float = 0.99
     rnn_init_imag: float = 0.1
     rnn_norm_input: bool = True
-    rnn_hidden_size: int = 128
+    rnn_hidden_size: int = 256
     rnn_out_size: int = 64
     rnn_pos_embedding: bool = False
     rnn_block: str = "rglru"
-    rnn_only_real: bool = True
+    rnn_only_real: bool = False
 
-    rnn_n_diag_blocks: int = 64
+    rnn_n_diag_blocks: int = 32
+
+    use_norm: bool = True
+    use_gating: bool = True
 
     base_dim: int = 64
     ff_expand: int = 2
-    rnn_last_scale: float = 1.0
-    rnn_n_layers: int = 4
-    use_gating: bool = True
+    rnn_last_scale: float = 0.125
+    rnn_n_layers: int = 8
     scan_implementation: str = "linear_pallas"
 
     @property
@@ -106,66 +109,54 @@ class UpPool(nn.Module):
 
 class ResBlock(nn.Module):
     H: ARHyperparams
-    expand: Union[int, None] = None
+    layer: nn.Module
     last_scale: float = 1.0
 
     @nn.compact
     def __call__(self, x, deterministic=False):
         bs, seq_len, dim = x.shape
-        expand = self.expand or self.H.ff_expand
-        z = nn.LayerNorm()(x)
-        z = jnp.copy(z)
-        z = nn.Dense(dim * expand)(z)
-        z = nn.gelu(z)
-        if self.H.use_gating:
-            z = nn.Dense(dim)(z) * nn.sigmoid(nn.Dense(dim)(z))
-        else:
-            z = nn.Dense(dim)(z)
+
+        z = nn.LayerNorm()(x) if self.H.use_norm else x
+        z = self.layer(z)
         z = z * self.last_scale
         return x + z, None
+
+
+class MLPBlock(nn.Module):
+    H: ARHyperparams
+    expand: int | None = None
+
+    @nn.compact
+    def __call__(self, x):
+        bs, seq_len, dim = x.shape
+        expand = self.expand or self.H.ff_expand
+        z = nn.Dense(dim * expand)(x)
+        if self.H.use_gating:
+            gated_x = nn.Dense(dim * expand)(x)
+            z = z * nn.gelu(gated_x)
+        else:
+            z = nn.gelu(z)
+        return nn.Dense(dim)(z)
 
 
 class RNNBlock(nn.Module):
     H: ARHyperparams
     d_out: int
-    bidirectional: bool = False
-    residual: bool = False
-    last_scale: float = 1.0
 
-    def setup(self):
+    @nn.compact
+    def __call__(self, x, h_prev=None):
         recurrent_block = get_recurrent_block(self.H)
-        self.forward = recurrent_block(
+        x_fwd, _ = recurrent_block(
             self.H,
             d_hidden=self.H.rnn_hidden_size,
             d_out=self.d_out,
-        )
-        if self.bidirectional:
-            self.backward = recurrent_block(
-                self.H,
-                d_hidden=self.H.rnn_hidden_size,
-                d_out=self.d_out,
-                reverse=True,
-            )
-        self.last_dense = nn.Dense(self.d_out)
+        )(x)
         if self.H.use_gating:
-            self.gate_dense = nn.Dense(self.d_out)
-        self.norm = nn.LayerNorm()
-
-    def __call__(self, x, h_prev=None):
-        assert h_prev is None or not self.bidirectional
-        identity = x
-        x = self.norm(x)
-        x_fwd, h_next = self.forward(x, h_prev)
-        x = (x_fwd + self.backward(x)[0]) / 2 if self.bidirectional else x_fwd
-
-        x = nn.gelu(x)
-        if self.H.use_gating
-            x = self.last_dense(x) * nn.sigmoid(self.gate_dense(identity))
+            gated_x = nn.Dense(self.d_out)(x)
+            x = x_fwd * nn.gelu(gated_x)
         else:
-            x = self.last_dense(x)
-        x = x * self.last_scale
-        x = x + identity if self.residual else x
-        return x, h_next
+            x = nn.gelu(x_fwd)
+        return nn.Dense(self.d_out)(x)
 
 
 class ARModel(nn.Module):
@@ -178,6 +169,20 @@ class ARModel(nn.Module):
         )
         self.cls_mlp = nn.Dense(self.H.data_num_cats)
         self.norm = nn.LayerNorm()
+
+        def _rnn_block(d_out, last_scale=1.0):
+            return ResBlock(
+                self.H,
+                layer=RNNBlock(self.H, d_out),
+                last_scale=last_scale,
+            )
+
+        def _mlp_block(expand=None, last_scale=1.0):
+            return ResBlock(
+                self.H,
+                layer=MLPBlock(self.H, expand=expand),
+                last_scale=last_scale,
+            )
 
         d_layers = []
         model_dim = self.H.base_dim
@@ -195,14 +200,12 @@ class ARModel(nn.Module):
         c_layers = []
         for _ in range(self.H.rnn_n_layers):
             c_layers.append(
-                RNNBlock(
-                    self.H,
+                _rnn_block(
                     model_dim,
-                    residual=True,
                     last_scale=self.H.rnn_last_scale,
                 )
             )
-            c_layers.append(ResBlock(self.H, last_scale=self.H.rnn_last_scale))
+            c_layers.append(_mlp_block(last_scale=self.H.rnn_last_scale))
 
         u_layers = []
         for p, expand in zip(
@@ -222,14 +225,12 @@ class ARModel(nn.Module):
 
             for _ in range(self.H.rnn_n_layers):
                 block.append(
-                    RNNBlock(
-                        self.H,
+                    _rnn_block(
                         model_dim,
-                        residual=True,
                         last_scale=self.H.rnn_last_scale,
                     )
                 )
-                block.append(ResBlock(self.H, last_scale=self.H.rnn_last_scale))
+                block.append(_mlp_block(last_scale=self.H.rnn_last_scale))
             u_layers.append(block)
 
         self.d_layers = d_layers
@@ -263,7 +264,7 @@ class ARModel(nn.Module):
                     outputs.append(x)
             x = x + outputs.pop()
 
-        # x = self.norm(x)
+        x = self.norm(x)
         x = jnp.reshape(
             self.cls_mlp(x),
             (

models/rnn.py
@@ -147,17 +147,8 @@ def stable_sqrt_bwd(
     g: jax.Array,
 ) -> tuple[jax.Array]:  # pylint: disable=g-one-element-tuple
     (x,) = res
-    if isinstance(x, complex_lib.Complex):
-        magnitude = jnp.sqrt(x.real**2 + x.imag**2)
-        # TODO: this part not working for jitted functions, not sure why
-        rescale = jnp.min(
-            jnp.ones(magnitude.shape), (1 / (magnitude * (4 * max_gradient**2)))
-        )
-        x_pre = x * rescale
-    else:
-        x_pre = jnp.maximum(x, 1 / (4 * max_gradient**2))
-    return jax.vjp(complex_lib.sqrt, x_pre)[1](g)
-
+    x_pre = jnp.maximum(x, 1 / (4 * max_gradient**2))
+    return jax.vjp(jnp.sqrt, x_pre)[1](g)
 
 sqrt_bound_derivative.defvjp(stable_sqrt_fwd, stable_sqrt_bwd)
 
@@ -423,14 +414,12 @@ class RGLRU(nn.Module):
 
         log_a = -8.0 * gate_a * complex_lib.softplus(a_real_param)
         if self.H.rnn_only_real:
-            a, a_squared = complex_lib.exp(log_a), complex_lib.exp(2 * log_a)
+            a = complex_lib.exp(log_a)
         else:
             log_a_imag = a_imag_param * gate_a
             log_a_complex = real_imag_complex(self.H, log_a, log_a_imag)
-            a, a_squared = (
-                complex_lib.exp(log_a_complex),
-                complex_lib.exp(2 * log_a_complex),
-            )
+            a = complex_lib.exp(log_a_complex)
+
         x = merged_to_complex(self.H, x)
         h_prev = (
             self.merged_to_complex(self.H, h_prev)
@@ -442,8 +431,10 @@ class RGLRU(nn.Module):
         # TODO: placement of norm corresponding to RGLRU
         # reconsider doing it before gating
         if self.H.rnn_norm_input:
-            # x = sqrt_bound_derivative(1 - a_squared, 1000) * x
-            x = complex_lib.sqrt(1 - a_squared) * x
+            a_squared = complex_lib.abs_squared(a)
+            x = sqrt_bound_derivative(1 - a_squared, 200) * x
+            # x = complex_lib.sqrt(1 - a_squared) * x
+            # x = jnp.sqrt(1 - complex_lib.abs_squared(a)) * x
 
         h, h_last = scan.linear_scan(
             x=x,
@@ -484,8 +475,6 @@ class RNNBlock(nn.Module):
                 reverse=True,
             )
         self.last_dense = nn.Dense(self.d_out)
-        if self.H.use_gating:
-            self.gate_dense = nn.Dense(self.d_out)
         self.norm = nn.LayerNorm()
 
     def __call__(self, x):
@@ -495,11 +484,7 @@ class RNNBlock(nn.Module):
         x = (x_fwd + self.backward(x)[0]) / 2 if self.bidirectional else x_fwd
         # x = x + identity if self.residual else x
 
-        x = nn.gelu(x)
-        if self.H.use_gating:
-            x = self.last_dense(x) * nn.sigmoid(self.gate_dense(x))
-        else:
-            x = self.last_dense(x)
+        x = self.last_dense(nn.gelu(x))
         x = x * self.last_scale
         x = x + identity if self.residual else x
         return x