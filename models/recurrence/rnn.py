c7a88e6b4e81b53ef73b763714b42efb73313c17
Jamie Townsend <jamiehntownsend@gmail.com>
2025-05-13 12:05:45 +0000

    Fix sharding spec in rnn.py

--- models/recurrence/rnn.py
+++ models/recurrence/rnn.py
@@ -9,24 +9,22 @@ from models.recurrence.common import (
     get_sinusoidal_embeddings,
     sqrt_bound_derivative,
 )
-from models.recurrence.hps import RNNHyperparams
-
-_mesh = jax.make_mesh((jax.device_count(),), ("batch",))
-SHARDING_SPEC = pallas.ShardingSpec(mesh=_mesh, batch_axis_name="batch")
+from hps import Hyperparams
 
 
 class RNN(nn.Module):
-    H: RNNHyperparams
+    H: Hyperparams
     d_hidden: int
     d_out: int
     reverse: bool = False
 
     @nn.compact
     def __call__(self, x, h_prev=None):
+        H_rnn = self.H.rnn
         batch_size, seq_len, _ = x.shape
 
         def stable_init(rng, shape):
-            r_min, r_max = self.H.init_minval_real, self.H.init_maxval_real
+            r_min, r_max = H_rnn.init_minval_real, H_rnn.init_maxval_real
             u = jax.random.uniform(
                 key=rng, shape=shape, minval=r_min, maxval=r_max
             )
@@ -35,22 +33,25 @@ class RNN(nn.Module):
         a_logit = self.param("a_logit", stable_init, (self.d_hidden,))
         a = expit(a_logit)
 
-        if self.H.pos_embedding:
+        if H_rnn.pos_embedding:
             x = jnp.concatenate(
                 [x, get_sinusoidal_embeddings(batch_size, seq_len, 16)], -1
             )
         dx = nn.Dense(self.d_out)(x)
         x = nn.Dense(self.d_hidden)(x)
-        if self.H.input_norm:
+        if H_rnn.input_norm:
             x = sqrt_bound_derivative(1 - a**2, 200) * x
         a = jnp.broadcast_to(a, x.shape)
+        sharding_spec = pallas.ShardingSpec(
+            self.H._mesh, batch_axis_name="batch", sequence_axis_name="seq"
+        )
         h, h_last = scan.linear_scan(
             x=x,
             a=a,
             h0=h_prev,
             reverse=self.reverse,
-            scan_type=get_scan_implementation(self.H),
-            sharding_spec=SHARDING_SPEC,
+            scan_type=get_scan_implementation(H_rnn),
+            sharding_spec=sharding_spec,
             unroll=128,
         )
         return (dx + nn.Dense(self.d_out)(h)) / 2, h_last
