48673f292e579a60e003215620e2f5bd1bc1b7c7
daniel-gallo <danielsantoangel@gmail.com>
2025-06-23 14:56:18 +0200

    Get checkpoint id from argv

--- scripts/find-checkpoints.sh
+++ scripts/find-checkpoints.sh
@@ -3,8 +3,12 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/parallel.sh"
 
-tpus=("tpu" "tpu1" "tpu3" "tpu-pod-0" "tpu-pod-1")
-checkpoints="b577bff7|ec36b4d8"
+tpus=("tpu" "tpu1" "tpu3" "tpu-pod-0" "tpu-pod-1" "tpu-pod-2")
+if [ -z "$1" ]; then
+    echo "Usage: $0 <checkpoint_regex>"
+    exit 1
+fi
+checkpoints="$1"
 
 find_checkpoints()  {
     local tpu="$1"
