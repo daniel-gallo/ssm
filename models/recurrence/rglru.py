ddc7b3af15916187c98c85735aa21a10ceef5cad
konradszewczyk <konszewczyk@outlook.com>
2025-07-26 17:07:37 +0000

    Stabilise exponential param

--- models/recurrence/rglru.py
+++ models/recurrence/rglru.py
@@ -63,7 +63,7 @@ class RGLRU(nn.Module):
                 case "tanh":
                     theta = jnp.arctanh(scale * u)
                 case "exponential":
-                    theta = jnp.log(jnp.abs(scale * u))
+                    theta = jnp.log(jnp.abs(scale * u) + 1e-5)
                 case _:
                     raise ValueError(f"Unknown param_imag: {H_rnn.param_imag}")
             return theta
